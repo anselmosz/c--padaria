7efd31445fe363dacaf106f95d5681ba2e0eb7b0
anselmosz <anselmoma2005@outlook.com>
2025-04-17 11:52:40 -0300

    update repository / project arrays

--- ProjetoArrays/frmNumAleatorio.Designer.cs
+++ ProjetoArrays/frmNumAleatorio.Designer.cs
@@ -29,32 +29,169 @@
         private void InitializeComponent()
         {
             this.gpbValores = new System.Windows.Forms.GroupBox();
+            this.rdInteirosRndIntervalos = new System.Windows.Forms.RadioButton();
+            this.rdVariosInteirosRnd = new System.Windows.Forms.RadioButton();
+            this.rdFloatRnd = new System.Windows.Forms.RadioButton();
+            this.rdInteirosRnd = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNumMaxInput = new System.Windows.Forms.TextBox();
+            this.lblNumMax = new System.Windows.Forms.Label();
+            this.txtNumMinInput = new System.Windows.Forms.TextBox();
+            this.lblNumMin = new System.Windows.Forms.Label();
+            this.ltbNumerosGerados = new System.Windows.Forms.ListBox();
+            this.txtNumeroInput = new System.Windows.Forms.TextBox();
+            this.lblNumero = new System.Windows.Forms.Label();
             this.btnGerar = new System.Windows.Forms.Button();
             this.btnSair = new System.Windows.Forms.Button();
             this.btnLimpar = new System.Windows.Forms.Button();
-            this.lblNumero = new System.Windows.Forms.Label();
-            this.txtNumeroInput = new System.Windows.Forms.TextBox();
-            this.ltbNumerosGerados = new System.Windows.Forms.ListBox();
+            this.rdRepeticao = new System.Windows.Forms.RadioButton();
+            this.txtTamanho = new System.Windows.Forms.TextBox();
+            this.lblTamanho = new System.Windows.Forms.Label();
             this.gpbValores.SuspendLayout();
             this.SuspendLayout();
             // 
             // gpbValores
             // 
+            this.gpbValores.Controls.Add(this.txtTamanho);
+            this.gpbValores.Controls.Add(this.lblTamanho);
+            this.gpbValores.Controls.Add(this.rdRepeticao);
+            this.gpbValores.Controls.Add(this.rdInteirosRndIntervalos);
+            this.gpbValores.Controls.Add(this.rdVariosInteirosRnd);
+            this.gpbValores.Controls.Add(this.rdFloatRnd);
+            this.gpbValores.Controls.Add(this.rdInteirosRnd);
+            this.gpbValores.Controls.Add(this.label3);
+            this.gpbValores.Controls.Add(this.txtNumMaxInput);
+            this.gpbValores.Controls.Add(this.lblNumMax);
+            this.gpbValores.Controls.Add(this.txtNumMinInput);
+            this.gpbValores.Controls.Add(this.lblNumMin);
             this.gpbValores.Controls.Add(this.ltbNumerosGerados);
             this.gpbValores.Controls.Add(this.txtNumeroInput);
             this.gpbValores.Controls.Add(this.lblNumero);
             this.gpbValores.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.gpbValores.Location = new System.Drawing.Point(12, 12);
             this.gpbValores.Name = "gpbValores";
-            this.gpbValores.Size = new System.Drawing.Size(499, 358);
+            this.gpbValores.Size = new System.Drawing.Size(721, 333);
             this.gpbValores.TabIndex = 0;
             this.gpbValores.TabStop = false;
             this.gpbValores.Text = "Valores";
             // 
+            // rdInteirosRndIntervalos
+            // 
+            this.rdInteirosRndIntervalos.AutoSize = true;
+            this.rdInteirosRndIntervalos.Location = new System.Drawing.Point(17, 154);
+            this.rdInteirosRndIntervalos.Name = "rdInteirosRndIntervalos";
+            this.rdInteirosRndIntervalos.Size = new System.Drawing.Size(228, 24);
+            this.rdInteirosRndIntervalos.TabIndex = 10;
+            this.rdInteirosRndIntervalos.TabStop = true;
+            this.rdInteirosRndIntervalos.Text = "Inteiros Aleatórios Intervalos";
+            this.rdInteirosRndIntervalos.UseVisualStyleBackColor = true;
+            this.rdInteirosRndIntervalos.CheckedChanged += new System.EventHandler(this.rdInteirosRndIntervalos_CheckedChanged);
+            // 
+            // rdVariosInteirosRnd
+            // 
+            this.rdVariosInteirosRnd.AutoSize = true;
+            this.rdVariosInteirosRnd.Location = new System.Drawing.Point(17, 124);
+            this.rdVariosInteirosRnd.Name = "rdVariosInteirosRnd";
+            this.rdVariosInteirosRnd.Size = new System.Drawing.Size(204, 24);
+            this.rdVariosInteirosRnd.TabIndex = 9;
+            this.rdVariosInteirosRnd.TabStop = true;
+            this.rdVariosInteirosRnd.Text = "Vários Inteiros Aleatórios";
+            this.rdVariosInteirosRnd.UseVisualStyleBackColor = true;
+            this.rdVariosInteirosRnd.CheckedChanged += new System.EventHandler(this.rdVariosInteirosRnd_CheckedChanged);
+            // 
+            // rdFloatRnd
+            // 
+            this.rdFloatRnd.AutoSize = true;
+            this.rdFloatRnd.Location = new System.Drawing.Point(17, 184);
+            this.rdFloatRnd.Name = "rdFloatRnd";
+            this.rdFloatRnd.Size = new System.Drawing.Size(191, 24);
+            this.rdFloatRnd.TabIndex = 9;
+            this.rdFloatRnd.TabStop = true;
+            this.rdFloatRnd.Text = "Gerar Floats Aleatórios";
+            this.rdFloatRnd.UseVisualStyleBackColor = true;
+            // 
+            // rdInteirosRnd
+            // 
+            this.rdInteirosRnd.AutoSize = true;
+            this.rdInteirosRnd.Location = new System.Drawing.Point(17, 94);
+            this.rdInteirosRnd.Name = "rdInteirosRnd";
+            this.rdInteirosRnd.Size = new System.Drawing.Size(155, 24);
+            this.rdInteirosRnd.TabIndex = 8;
+            this.rdInteirosRnd.TabStop = true;
+            this.rdInteirosRnd.Text = "Inteiros Aleatórios";
+            this.rdInteirosRnd.UseVisualStyleBackColor = true;
+            this.rdInteirosRnd.CheckedChanged += new System.EventHandler(this.rdInteirosRnd_CheckedChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Selecione";
+            // 
+            // txtNumMaxInput
+            // 
+            this.txtNumMaxInput.Location = new System.Drawing.Point(281, 254);
+            this.txtNumMaxInput.Name = "txtNumMaxInput";
+            this.txtNumMaxInput.Size = new System.Drawing.Size(119, 26);
+            this.txtNumMaxInput.TabIndex = 6;
+            // 
+            // lblNumMax
+            // 
+            this.lblNumMax.AutoSize = true;
+            this.lblNumMax.Location = new System.Drawing.Point(277, 231);
+            this.lblNumMax.Name = "lblNumMax";
+            this.lblNumMax.Size = new System.Drawing.Size(123, 20);
+            this.lblNumMax.TabIndex = 5;
+            this.lblNumMax.Text = "Numero Máximo";
+            // 
+            // txtNumMinInput
+            // 
+            this.txtNumMinInput.Location = new System.Drawing.Point(281, 202);
+            this.txtNumMinInput.Name = "txtNumMinInput";
+            this.txtNumMinInput.Size = new System.Drawing.Size(119, 26);
+            this.txtNumMinInput.TabIndex = 4;
+            // 
+            // lblNumMin
+            // 
+            this.lblNumMin.AutoSize = true;
+            this.lblNumMin.Location = new System.Drawing.Point(277, 179);
+            this.lblNumMin.Name = "lblNumMin";
+            this.lblNumMin.Size = new System.Drawing.Size(119, 20);
+            this.lblNumMin.TabIndex = 3;
+            this.lblNumMin.Text = "Numero Minimo";
+            // 
+            // ltbNumerosGerados
+            // 
+            this.ltbNumerosGerados.FormattingEnabled = true;
+            this.ltbNumerosGerados.ItemHeight = 20;
+            this.ltbNumerosGerados.Location = new System.Drawing.Point(468, 37);
+            this.ltbNumerosGerados.Name = "ltbNumerosGerados";
+            this.ltbNumerosGerados.Size = new System.Drawing.Size(226, 264);
+            this.ltbNumerosGerados.TabIndex = 2;
+            // 
+            // txtNumeroInput
+            // 
+            this.txtNumeroInput.Location = new System.Drawing.Point(281, 122);
+            this.txtNumeroInput.Name = "txtNumeroInput";
+            this.txtNumeroInput.Size = new System.Drawing.Size(119, 26);
+            this.txtNumeroInput.TabIndex = 1;
+            // 
+            // lblNumero
+            // 
+            this.lblNumero.AutoSize = true;
+            this.lblNumero.Location = new System.Drawing.Point(277, 99);
+            this.lblNumero.Name = "lblNumero";
+            this.lblNumero.Size = new System.Drawing.Size(123, 20);
+            this.lblNumero.TabIndex = 0;
+            this.lblNumero.Text = "Insira o múmero";
+            // 
             // btnGerar
             // 
             this.btnGerar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnGerar.Location = new System.Drawing.Point(12, 376);
+            this.btnGerar.Location = new System.Drawing.Point(29, 351);
             this.btnGerar.Name = "btnGerar";
             this.btnGerar.Size = new System.Drawing.Size(135, 44);
             this.btnGerar.TabIndex = 1;
@@ -65,7 +202,7 @@
             // btnSair
             // 
             this.btnSair.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSair.Location = new System.Drawing.Point(376, 376);
+            this.btnSair.Location = new System.Drawing.Point(393, 351);
             this.btnSair.Name = "btnSair";
             this.btnSair.Size = new System.Drawing.Size(135, 44);
             this.btnSair.TabIndex = 2;
@@ -76,7 +213,7 @@
             // btnLimpar
             // 
             this.btnLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnLimpar.Location = new System.Drawing.Point(196, 376);
+            this.btnLimpar.Location = new System.Drawing.Point(213, 351);
             this.btnLimpar.Name = "btnLimpar";
             this.btnLimpar.Size = new System.Drawing.Size(135, 44);
             this.btnLimpar.TabIndex = 3;
@@ -84,36 +221,39 @@
             this.btnLimpar.UseVisualStyleBackColor = true;
             this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
             // 
-            // lblNumero
+            // rdRepeticao
             // 
-            this.lblNumero.AutoSize = true;
-            this.lblNumero.Location = new System.Drawing.Point(29, 45);
-            this.lblNumero.Name = "lblNumero";
-            this.lblNumero.Size = new System.Drawing.Size(123, 20);
-            this.lblNumero.TabIndex = 0;
-            this.lblNumero.Text = "Insira o múmero";
+            this.rdRepeticao.AutoSize = true;
+            this.rdRepeticao.Location = new System.Drawing.Point(17, 214);
+            this.rdRepeticao.Name = "rdRepeticao";
+            this.rdRepeticao.Size = new System.Drawing.Size(200, 24);
+            this.rdRepeticao.TabIndex = 11;
+            this.rdRepeticao.TabStop = true;
+            this.rdRepeticao.Text = "Gerar inteiros Repetição";
+            this.rdRepeticao.UseVisualStyleBackColor = true;
+            this.rdRepeticao.CheckedChanged += new System.EventHandler(this.rdRepeticao_CheckedChanged);
             // 
-            // txtNumeroInput
+            // txtTamanho
             // 
-            this.txtNumeroInput.Location = new System.Drawing.Point(33, 68);
-            this.txtNumeroInput.Name = "txtNumeroInput";
-            this.txtNumeroInput.Size = new System.Drawing.Size(119, 26);
-            this.txtNumeroInput.TabIndex = 1;
+            this.txtTamanho.Location = new System.Drawing.Point(281, 70);
+            this.txtTamanho.Name = "txtTamanho";
+            this.txtTamanho.Size = new System.Drawing.Size(119, 26);
+            this.txtTamanho.TabIndex = 13;
             // 
-            // ltbNumerosGerados
+            // lblTamanho
             // 
-            this.ltbNumerosGerados.FormattingEnabled = true;
-            this.ltbNumerosGerados.ItemHeight = 20;
-            this.ltbNumerosGerados.Location = new System.Drawing.Point(236, 68);
-            this.ltbNumerosGerados.Name = "ltbNumerosGerados";
-            this.ltbNumerosGerados.Size = new System.Drawing.Size(226, 264);
-            this.ltbNumerosGerados.TabIndex = 2;
+            this.lblTamanho.AutoSize = true;
+            this.lblTamanho.Location = new System.Drawing.Point(277, 47);
+            this.lblTamanho.Name = "lblTamanho";
+            this.lblTamanho.Size = new System.Drawing.Size(76, 20);
+            this.lblTamanho.TabIndex = 12;
+            this.lblTamanho.Text = "Tamanho";
             // 
             // frmNumAleatorio
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(523, 435);
+            this.ClientSize = new System.Drawing.Size(747, 405);
             this.Controls.Add(this.btnLimpar);
             this.Controls.Add(this.btnSair);
             this.Controls.Add(this.btnGerar);
@@ -135,5 +275,17 @@
         private System.Windows.Forms.ListBox ltbNumerosGerados;
         private System.Windows.Forms.TextBox txtNumeroInput;
         private System.Windows.Forms.Label lblNumero;
+        private System.Windows.Forms.RadioButton rdInteirosRndIntervalos;
+        private System.Windows.Forms.RadioButton rdVariosInteirosRnd;
+        private System.Windows.Forms.RadioButton rdFloatRnd;
+        private System.Windows.Forms.RadioButton rdInteirosRnd;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNumMaxInput;
+        private System.Windows.Forms.Label lblNumMax;
+        private System.Windows.Forms.TextBox txtNumMinInput;
+        private System.Windows.Forms.Label lblNumMin;
+        private System.Windows.Forms.RadioButton rdRepeticao;
+        private System.Windows.Forms.TextBox txtTamanho;
+        private System.Windows.Forms.Label lblTamanho;
     }
 }
--- ProjetoArrays/frmNumAleatorio.cs
+++ ProjetoArrays/frmNumAleatorio.cs
@@ -15,39 +15,154 @@ namespace ProjetoArrays
         public frmNumAleatorio()
         {
             InitializeComponent();
+            disableTxtBoxes();
         }
 
         Random rnd = new Random();
+         
 
-        public void metodo()
+        public void disableTxtBoxes()
         {
-
+            txtNumeroInput.Enabled = false;
+            txtTamanho.Enabled = false; 
+            txtNumMinInput.Enabled = false;
+            txtNumMaxInput.Enabled = false;
         }
 
         private void btnGerar_Click(object sender, EventArgs e)
         {   
-            try
+            if (rdInteirosRnd.Checked)
             {
-                int valor = int.Parse(txtNumeroInput.Text);
-                int num = rnd.Next(valor);
-                ltbNumerosGerados.Items.Add(num);
+                try
+                {
+                    int valor = int.Parse(txtNumeroInput.Text);
+                    ltbNumerosGerados.Items.Add(rnd.Next(valor));
+                    txtNumeroInput.Focus();
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Favor inserir um número inteiro");
+                    txtNumeroInput.Clear();
+                    txtNumeroInput.Focus();
+                }
             }
-            catch (Exception)
+
+            if (rdVariosInteirosRnd.Checked)
             {
-                MessageBox.Show("error")
+                try
+                {
+                    int valor = int.Parse(txtNumeroInput.Text);
+
+                    for (int i = 0; i < valor; i++)
+                    {
+                        ltbNumerosGerados.Items.Add(rnd.Next(valor));
+                    }
+                    
+                    txtNumeroInput.Focus();
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Favor inserir um número inteiro");
+                    txtNumeroInput.Clear();
+                    txtNumeroInput.Focus();
+                }
+            }
+
+            if (rdInteirosRndIntervalos.Checked)
+            {
+                try
+                {
+                    int valor1 = int.Parse(txtNumMinInput.Text);
+                    int valor2 = int.Parse(txtNumMaxInput.Text);
+                    ltbNumerosGerados.Items.Add(rnd.Next(valor1, valor2));
+                    txtNumMinInput.Focus();
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Favor inserir um interalo de números inteiros");
+                    txtNumMinInput.Clear();
+                    txtNumMaxInput.Clear();
+                    txtNumMinInput.Focus();
+                }
+            }
+
+            if (rdFloatRnd.Checked)
+            {
+                try
+                {
+                    double num = rnd.NextDouble();
+                    ltbNumerosGerados.Items.Add(num);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Favor selecionar");
+                }
+            }
+
+            if (rdRepeticao.Checked)
+            {
+                try
+                {
+                    int valor = int.Parse(txtNumeroInput.Text);
+                    int tamanho = int.Parse(txtTamanho.Text);
+                    for (int i = 0; i < tamanho; i++)
+                    {
+                        ltbNumerosGerados.Items.Add(rnd.Next(valor));
+                    }
+                    ltbNumerosGerados.Items.Add("------");
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Favor inserir um interalo de números inteiros");
+                }
             }
         }
 
         private void btnLimpar_Click(object sender, EventArgs e)
         {
             ltbNumerosGerados.Items.Clear();
-            txtNumeroInput.Clear(); 
-            txtNumeroInput.Focus(); 
+            txtTamanho.Clear();
+            txtNumeroInput.Clear();
+            txtNumMinInput.Clear();
+            txtNumMaxInput.Clear();
         }
 
         private void btnSair_Click(object sender, EventArgs e)
         {
             Application.Exit();
+        }
+
+        private void rdInteirosRnd_CheckedChanged(object sender, EventArgs e)
+        {
+            txtNumeroInput.Enabled = true;
+            txtNumeroInput.Focus();
+            txtNumMinInput.Enabled = false;
+            txtNumMaxInput.Enabled = false;
+        }
+
+        private void rdVariosInteirosRnd_CheckedChanged(object sender, EventArgs e)
+        {
+            txtNumeroInput.Enabled = true;
+            txtNumeroInput.Focus();
+            txtNumMinInput.Enabled = false;
+            txtNumMaxInput.Enabled = false;
+        }
+
+        private void rdInteirosRndIntervalos_CheckedChanged(object sender, EventArgs e)
+        {
+            txtNumeroInput.Enabled = false;
+            txtNumMinInput.Enabled = true;
+            txtNumMaxInput.Enabled = true;
+            txtNumMinInput.Focus();
+        }
+
+        private void rdRepeticao_CheckedChanged(object sender, EventArgs e)
+        {
+            txtTamanho.Enabled = true;
+            txtTamanho.Focus();
+            txtNumeroInput.Enabled = true;
+            txtNumMinInput.Enabled = false;
+            txtNumMaxInput.Enabled = false;
         }
     }
 }
